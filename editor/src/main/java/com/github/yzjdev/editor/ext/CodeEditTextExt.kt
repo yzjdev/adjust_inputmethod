b9dcbc027eaad6ab07230690b1b0e035b5069264
yzjdev <2550962720@qq.com>
2025-08-23 22:38:13 +0800

    代码优化

--- editor/src/main/java/com/github/yzjdev/editor/ext/CodeEditTextExt.kt
+++
@@ -1,2 +0,0 @@
-package com.github.yzjdev.editor.ext
-
--- editor/src/main/java/com/github/yzjdev/editor/ext/DocumentEx.kt
+++
@@ -1,9 +0,0 @@
-package com.github.yzjdev.editor.ext
-
-import org.eclipse.jface.text.Document
-fun Document.getRealLineLength(line: Int) = getLineInformation(line).length
-
-fun Document.getLineStart(line: Int) = getLineOffset(line)
-
-fun Document.getLineEnd(line: Int) = getLineStart(line) + getRealLineLength(line)
-fun Document.getLineText(line: Int) = get(getLineStart(line), getRealLineLength(line))
